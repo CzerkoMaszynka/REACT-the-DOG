50f095ee7c7169280dae9e3f72711aa5a72be733
Damian <damian.czerech@gmail.com>
2019-11-28 09:43:03 +0100

    Updates

---
+++ precache-manifest.666797f9b7c3e78aad9b08453a097fac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2dd3f4053d4f450ec889a6eed5f674d",
+    "url": "/REACT-the-DOG/index.html"
+  },
+  {
+    "revision": "61105dbfb5681ef02492",
+    "url": "/REACT-the-DOG/static/css/main.869db6c0.chunk.css"
+  },
+  {
+    "revision": "b3b56b72044e6e469df5",
+    "url": "/REACT-the-DOG/static/js/2.f7e293b0.chunk.js"
+  },
+  {
+    "revision": "61105dbfb5681ef02492",
+    "url": "/REACT-the-DOG/static/js/main.a8ca9a13.chunk.js"
+  },
+  {
+    "revision": "bd8f3c1dc388ea50f00a",
+    "url": "/REACT-the-DOG/static/js/runtime-main.6a900b64.js"
+  }
+]);
